e910d2732e26e96ccd2c3e9cb2a484edda9a1122
Thomas Becker <thomas.becker00@gmail.com>
2018-12-31 19:19:57 +0100

    initial commit

---
+++ src/test/groovy/de/softwareschmied/homeintegrator/tempsensorserver/SensorServletSpec.groovy
@@ -0,0 +1,19 @@
+package de.softwareschmied.homeintegrator.tempsensorserver
+
+import spock.lang.Specification
+
+/**
+ * Created by Thomas Becker (thomas.becker00@gmail.com) on 2018-12-31.
+ */
+class SensorServletSpec extends Specification {
+    SensorServlet sensorServlet = new SensorServlet()
+
+    def "convert sensor set to json works"() {
+        given:
+        def sensors = [new Sensor("1", "22.444"), new Sensor("2", "23.222")] as Set
+        when:
+        def json = sensorServlet.getAsJson(sensors)
+        then:
+        json == "[{id: 1, value: 22.444},{id: 2, value: 23.222}]"
+    }
+}
